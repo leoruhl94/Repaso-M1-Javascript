d5cbc612af2750134d81368e07127ca440d63c93
leoruhl94 <leoruhl94@gmail.com>
2021-11-04 14:43:47 -0300

    .

--- DS.js
+++ DS.js
@@ -1,25 +1,38 @@
-function Node(value){
-    this.value = value;
-    this.next = null;
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
+
+function LinkedList() {
+  this.head = null;
+  this.length = 0;
+}
+
+LinkedList.prototype.add = function (value) {
+  let newNode = new Node(value);
+  if (!this.head) this.head = newNode;
+  else {
+    let currentNode = this.head;
+    if (currentNode) {
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
   }
-  
-  function LinkedList(){
-    this.head = null;
-    this.length = 0; 
-  }
-  
-  function OrdenedLinkedList(){
-    this.head = null;
-    this.length = 0; 
-  }
-  
-  
-  
-  module.exports = {
-    OrdenedLinkedList,
-    Queue,
-    Node,
-    LinkedList,
-    BinarySearchTree
-  };
-  
+  this.length++;
+  return newNode;
+};
+
+function OrderedLinkedList() {
+  this.head = null;
+  this.length = 0;
+}
+
+module.exports = {
+  OrderedLinkedList,
+  Queue,
+  Node,
+  LinkedList,
+  BinarySearchTree,
+};
