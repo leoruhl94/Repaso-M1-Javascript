f24ea03487c12074640ec0683d7c4dd5c192c82c
leoruhl94 <leoruhl94@gmail.com>
2021-11-04 14:30:35 -0300

    .

--- checkpoint.js
+++ checkpoint.js
@@ -1,3 +1,7 @@
+const {
+    LinkedList
+} = require('./DS.js')
+
 function prueba (){
     return 'probando'
 }
@@ -25,6 +29,61 @@ OLL
 
 
 */
+
+LinkedList.prototype.add = function (value){
+    let newNode = new Node(value);
+    if(this.head === null){
+        this.head = newNode;
+    } else {
+        let currentNode = this.head;
+        if(currentNode){
+            while(currentNode.next){
+                currentNode = currentNode.next; 
+            }
+            currentNode.next = newNode;
+        }
+    }
+    this.length++;
+    return this;
+}
+LinkedList.prototype.remove = function (){
+    if(!this.head) return null;
+    else if(!this.head.next){
+        let data = this.head.value;
+        this.head = null;
+        this.length--;
+        return data;
+    }else {
+        let currentNode = this.head;
+        let holdingPointer;
+        let data;
+        if(currentNode){
+            while(currentNode.next){
+                holdingPointer = currentNode;
+                currentNode = currentNode.next;
+            }
+            data = currentNode.value;
+            holdingPointer.next = null;
+            this.length--;
+            return data;
+        }
+    } 
+}
+LinkedList.prototype.search = function (value){
+    let currentNode = this.head;
+    while(currentNode){
+        if (typeof value === 'function'){
+            if(value(currentNode.value)===true)
+                return currentNode.value;
+        }  
+        if(currentNode.value === value)
+          return currentNode.value;
+        else   
+            currentNode = currentNode.next;
+        }
+    return null; 
+}
+
 
 
 
